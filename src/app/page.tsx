6684aa5c2e7371ab3350c7ba1dd4095dfb21afe5
Ekaterina Baliasnikova <22520904+catherineriver@users.noreply.github.com>
2025-03-05 17:32:52 +0200

    add  landing mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,21 @@ import React from "react";
 const LandingPage = () => {
     return (
         <Container maxW="100vw" minH="100vh" p={0}>
-            <Flex h="100vh">
+            <Flex h="100vh"
+                  direction={{ base: "column", md: "row" }}
+                  alignItems="center"
+                  justifyContent={{ base: "space-between", md: "center" }}
+                  px={{ base: 4, md: 10 }}
+                  py={{ base: 6, md: 0 }}>
                 {/* Левая секция (61.8%) */}
-                <Box w="61.8%" bg="white" p={10} display="flex" flexDirection="column" justifyContent="center">
+                <Box w={{ base: "100%", md: "61.8%" }}
+                     textAlign={{ base: "left", md: "left" }}
+                     display="flex"
+                     flexDirection="column"
+                     justifyContent="center"
+                >
                     <Flex alignItems="center" mb={5}>
-                        <Box w="86px" h="86px" bg="blue.800" borderRadius="full" display="flex" alignItems="center" justifyContent="center">
+                        <Box w={{ base: "48px", md: "86px" }} h={{ base: "48px", md: "86px" }} bg="blue.800" borderRadius="full" display="flex" alignItems="center" justifyContent="center">
                             <svg width="85" height="86" viewBox="0 0 85 86" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M84.0163 43C84.0163 54.1429 79.5905 64.8294 71.7124 72.7086C63.8343 80.5878 53.1494 85.0142 42.0082 85.0142C33.6997 85.0142 25.5779 82.5501 18.6697 77.9336C11.7615 73.317 6.3772 66.7553 3.1977 59.0782C0.0182056 51.4011 -0.813701 42.9534 0.807192 34.8035C2.42809 26.6535 6.42898 19.1673 12.3039 13.2915C18.1789 7.41574 25.664 3.41427 33.8128 1.79315C41.9616 0.172022 50.408 1.00405 58.084 4.184C65.76 7.36396 72.3207 12.749 76.9367 19.6582C81.5526 26.5674 84.0163 34.6904 84.0163 43Z" fill="#000086"/>
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M47.3915 47.0513L12.7115 27.3721L29.8775 62.463L47.3915 47.0513Z" fill="white"/>
@@ -18,7 +28,7 @@ const LandingPage = () => {
 
                         </Box>
                     </Flex>
-                    <Heading as="h1" fontSize="84px" fontWeight="bold" lineHeight="1.1" fontFamily="Futura">
+                    <Heading as="h1" fontSize={{ base: "38px", md: "84px" }} fontWeight="bold" lineHeight="1.1" fontFamily="Futura">
                         Связываем журналистов и&nbsp;фотографов с&nbsp;независимыми медиа по всему миру.
                     </Heading>
                 </Box>
@@ -28,20 +38,25 @@ const LandingPage = () => {
                     top={0}
                     bottom={0}
                     width="2px"
-                    backgroundImage= "radial-gradient(circle, {colors.neutral} 1px, transparent 1px)"
-                    backgroundPosition= "left top"
-                    backgroundRepeat= "repeat-y"
-                    backgroundSize= "2px 4px"
+                    backgroundImage="radial-gradient(circle, #CCC 1px, transparent 1px)"
+                    backgroundRepeat="repeat-y"
+                    backgroundSize="2px 4px"
                     transform="translateX(-50%)"
                     display={{ base: "none", md: "block" }}
                 />
                 {/* Правая секция (38.2%) */}
-                <Box w="38.2%" p={10} display="flex" flexDirection="column" justifyContent="center">
+                <Box
+                    w={{ base: "100%", md: "38.2%" }}
+                    display="flex"
+                    flexDirection="column"
+                    textAlign={{ base: "center", md: "left" }}
+                    mt={{ base: "auto", md: 0 }}
+                >
                     <Flex direction="column" alignItems="flex-start">
                         <Link
                             as={NextLink}
                             href="/authors"
-                            fontSize="38px"
+                            fontSize={{ base: "24px", md: "38px" }}
                             fontWeight="medium"
                             display="flex"
                             alignItems="center"
@@ -63,7 +78,7 @@ const LandingPage = () => {
                         <Link
                             as={NextLink}
                             href="/pitches"
-                            fontSize="38px"
+                            fontSize={{ base: "24px", md: "38px" }}
                             fontWeight="medium"
                             display="flex"
                             alignItems="center"
